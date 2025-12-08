62be474be56ef9b3eee1a2e0d210550291ac52bf
thiagoDOTjpeg <thiago.gritti12@gmail.com>
2025-12-08 18:43:28 -0300

    feat(types): adição de dto para criar comentários

--- packages/types/dto/index.ts
+++ packages/types/dto/index.ts
@@ -3,6 +3,7 @@ export * from "./auth/register-auth.dto"
 export * from "./pagination/pagination-query.dto"
 export * from "./pagination/pagination-result.dto"
 export * from "./tasks/assign-task.dto"
+export * from "./tasks/create-comment.dto"
 export * from "./tasks/create-task.dto"
 export * from "./users/update-user.dto"
 
