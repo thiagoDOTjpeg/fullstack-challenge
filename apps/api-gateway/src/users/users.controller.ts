1884d64442e42bde3dfec93f9fc3e4e1fca75534
thiagoDOTjpeg <thiago.gritti12@gmail.com>
2025-12-15 18:51:08 -0300

    feat(gateway): adicionar endpoints para getAllUsers

--- apps/api-gateway/src/users/users.controller.ts
+++ apps/api-gateway/src/users/users.controller.ts
@@ -1,4 +1,3 @@
-import { ResponseUserDto } from '@challenge/types';
 import { Controller, Get, Inject, Query, UseGuards } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { AuthGuard } from '@nestjs/passport';
@@ -15,11 +14,14 @@ export class UsersController {
   @ApiOperation({ summary: 'Batch buscar usuários por IDs' })
   @ApiQuery({ name: 'ids', required: true, description: 'Lista de IDs separada por vírgula' })
   @ApiResponse({ status: 200, description: 'Usuários retornados.' })
-  async getManyByIds(@Query('ids') idsParam: string): Promise<ResponseUserDto[]> {
+  async getManyByIds(@Query('ids') idsParam: string) {
     const ids = (idsParam ?? '')
       .split(',')
       .map((s) => s.trim())
       .filter((s) => s.length > 0);
-    return this.authClient.send('users.getManyByIds', { ids }).toPromise();
+    if (!ids.length) {
+      return this.authClient.send('users.getAll', {});
+    }
+    return this.authClient.send('users.getManyByIds', { ids });
   }
 }
