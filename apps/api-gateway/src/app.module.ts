1931b8822680757ad48ae3849c5987c7e2259a3a
thiagoDOTjpeg <thiago.gritti12@gmail.com>
2025-12-16 12:02:38 -0300

    feat(gateway): adiciona rate limitting

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AuthModule } from './auth/auth.module';
 import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TasksModule, AuthModule, UsersModule],
+  imports: [
+    ThrottlerModule.forRoot({
+      errorMessage: "Muitas requisições num curto período. Tente novamente em alguns instantes.",
+      throttlers: [{ limit: 10, ttl: 1000 }]
+    }), TasksModule, AuthModule, UsersModule],
   controllers: [],
-  providers: [],
+  providers: [{
+    provide: APP_GUARD,
+    useClass: ThrottlerGuard
+  }],
 })
 export class AppModule { }
