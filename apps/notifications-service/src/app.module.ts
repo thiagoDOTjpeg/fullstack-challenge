aded96ad0e5fb2722b8b77121396aabfadd3868a
thiagoDOTjpeg <thiago.gritti12@gmail.com>
2025-12-16 13:44:31 -0300

    feat(notification): adiciona loggin com nestjs/pino

--- apps/notifications-service/src/app.module.ts
+++ apps/notifications-service/src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from 'db/datasource';
+import { LoggerModule } from 'nestjs-pino';
 import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), NotificationsModule],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: process.env.NODE_ENV !== "production" ? { target: "pino-pretty" } : undefined
+      }
+    }),
+    TypeOrmModule.forRoot(dataSourceOptions), NotificationsModule],
   controllers: [],
   providers: [],
 })
