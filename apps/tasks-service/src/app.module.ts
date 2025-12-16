458d55ec0aedc02f52d4083e1d66c605c14c8a00
thiagoDOTjpeg <thiago.gritti12@gmail.com>
2025-12-16 13:43:10 -0300

    feat(task): adiciona loggin com nestjs/pino

--- apps/tasks-service/src/app.module.ts
+++ apps/tasks-service/src/app.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from 'db/datasource';
+import { LoggerModule } from 'nestjs-pino';
 import { CommentModule } from './comment/comment.module';
-import { TaskModule } from './task/task.module';
 import { HistoryModule } from './history/history.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: process.env.NODE_ENV !== "production" ? { target: "pino-pretty" } : undefined
+      }
+    }),
     ConfigModule.forRoot({
       isGlobal: true
     }),
--- apps/tasks-service/src/main.ts
+++ apps/tasks-service/src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
+import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  app.useLogger(app.get(Logger))
+
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
     options: {
